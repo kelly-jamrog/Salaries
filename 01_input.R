43cbd53a16d9433214385cb37c02a9db0d4ccf1c
Kelly-Jamrog <kjmarog@wesleyan.edu>
2018-09-11 16:03:54 -0400

    Expaneded readme, notes on input, and labels on graph

--- 01_input.R
+++ 01_input.R
@@ -1,5 +1,7 @@
 # input salaries data
 
+# This is a dataset the describes the 9 month salary for male
+# and female professors at a single university in 2008-2009
 library(carData)
 data(Salaries)
 
--- 03_graphs.R
+++ 03_graphs.R
@@ -4,4 +4,6 @@ library(ggplot2)
 ggplot(data = salaries,
 	aes(x = salary)) +
 	geo_histogram()  +
-	facet_grid(rank ~ sex) # a histogram for each combination of rank and sex
+	facet_grid(rank ~ sex) + # a histogram for each combination of rank and sex
+	label(title = "Salary by Sex and Rank") +
+	theme_minimal()
